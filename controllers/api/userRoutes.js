b9f0650dbd836a7635d070aba12e085ba096ceec
unknown <jessedraper2@gmail.com>
2021-12-04 15:54:59 -0700

    work on server.js and config

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,7 +4,8 @@ const { Router } = require('express')
 const app = require('express').Router();
 app.get('/', async (req,res) => {
     try{
-    const getUser= await User.findByPk(req.params.id)
+    const getUser = await User.findByPk(req.params.id);
+    const thisUser = getUser.get({plain: true});
     res.render('homepage', User)
 } catch (err){
     res.status(400).json(err)
